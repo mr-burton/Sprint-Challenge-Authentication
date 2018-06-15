fac6772feec9a2b066222165373e8d84e1007d72
Lavell Burton <taylormade800@gmail.com>
2018-06-15 10:24:46 -0700

    update to files

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -8,7 +8,7 @@ const createUser = (req, res) => {
   User
     .create({ username, password })
     .then(response => res.status(201).json({ data: response }))
-    .catch(err => res.status(500).json({ message: err }));
+    .catch(err => res.status(500).json({ message: err.message }));
 };
 
 module.exports = {
