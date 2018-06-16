bf334fd664f8dd8c6184674e916e99ab20ae8d30
Lavell Burton <taylormade800@gmail.com>
2018-06-15 20:46:51 -0700

    added react client

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,7 +1,7 @@
 const User = require('../models/userModels');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const secret = 'europe is my favorite country, i like cold weather!';
+const { mysecret } = require('../../config');
 
 
 function generateToken(user) {
@@ -22,7 +22,7 @@ const createUser = (req, res) => {
       const token = generateToken(user);
       res.status(201).json({ data: response })
     })
-    .catch(err => res.status(500).json({ message: err.message }));
+    .catch(err => res.status(500).json({ message: err }));
 };
 
 module.exports = {
