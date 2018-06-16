be1fb5f36e4b2acdbc6de0fc5ad4c6e0c3eec7f9
Lavell Burton <taylormade800@gmail.com>
2018-06-15 20:33:55 -0700

    updating for token implementation

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,5 +1,16 @@
 const User = require('../models/userModels');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const secret = 'europe is my favorite country, i like cold weather!';
+
+
+function generateToken(user) {
+  const options = {
+    expiresIn: '40m',
+  }
+  const payload = { name: user.username };
+  return jwt.sign(payload, secret, options);
+}
 
 const createUser = (req, res) => {
   const { username, password } = req.body;
@@ -7,7 +18,10 @@ const createUser = (req, res) => {
   // our pre save hook should kick in here saving this user to the DB with an encrypted password.
   User
     .create({ username, password })
-    .then(response => res.status(201).json({ data: response }))
+    .then(response => {
+      const token = generateToken(user);
+      res.status(201).json({ data: response })
+    })
     .catch(err => res.status(500).json({ message: err.message }));
 };
 
